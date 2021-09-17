bcf834128dc74f614885423451ddea0e0ba5754f
Daria Górecka <dmgorecka@gmail.com>
2021-09-17 08:57:20 +0200

    Remove Link class

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import styles from './Tables.module.scss';
 import {Link} from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
 
  const Tables = () => (
    <div className={styles.component}>
-     <h2>Tables view</h2>
-     <Link to={`${process.env.PUBLIC_URL}/tables/booking/new`} activeClassName='active'>New booking table</Link>
-     <Link to={`${process.env.PUBLIC_URL}/tables/booking/123abc`} activeClassName='active'>Booked table 123abc</Link>
-     <Link to={`${process.env.PUBLIC_URL}/tables/event/new`} activeClassName='active'>New booking event</Link>
-     <Link to={`${process.env.PUBLIC_URL}/tables/event/456def`} activeClassName='active'>Booked event 456def</Link>
+     <Typography variant="h4">Tables view</Typography>
+     <Typography variant="subtitle1" sx={{ display: 'block' }} component={Link} to={`${process.env.PUBLIC_URL}/tables/booking/new`} >New booking table</Typography>
+     <Typography variant="subtitle1" sx={{ display: 'block' }} component={Link} to={`${process.env.PUBLIC_URL}/tables/booking/123abc`} >Booked table 123abc</Typography>
+     <Typography variant="subtitle1" sx={{ display: 'block' }} component={Link} to={`${process.env.PUBLIC_URL}/tables/event/new`} >New booking event</Typography>
+     <Typography variant="subtitle1" sx={{ display: 'block' }} component={Link} to={`${process.env.PUBLIC_URL}/tables/event/456def`} >Booked event 456def</Typography>
 
    </div>
  );
